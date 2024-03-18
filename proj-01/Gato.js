2d5a0478b7a91b97bcc3ff1414a8c6ff85b42e3a
Vitor Gabriel da Silva Santos <commercial.vitorgabrieL@gmail.com>
2024-03-18 08:19:11 -0300

    Arquivo Gato adicionado

---
+++ proj-01/Gato.js
@@ -0,0 +1,20 @@
+import { View, Text } from "react-native";
+
+const Gato = () => {
+	return (
+		<View>
+			<Text>Meu nome é VITOR</Text>
+		</View>
+	)
+}
+
+export default Gato;
+
+/* export default function Gato(){
+	return(
+		<View>
+
+		</View>
+	)
+}
+ */
