fc1c86b5ed056ee9d77d74ecf1f7c72fbb8ee1f3
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2024-04-01 09:09:44 -0300

    Adição de classes

--- vixz/App.js
+++ vixz/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -12,10 +12,11 @@ export default function App() {
 
         </View>
         <TextInput placeholder='E-mail ou nome de usuário' style={styles.ipt}/>
-        <TextInput placeholder='Senha' style={styles.ipt}/>
-        <Button title="Entrar" />
+        <TextInput placeholder='Senha' secureTextEntry="true" style={styles.ipt}/>
+        <TouchableOpacity style={[styles.btn, styles.borderRadius100]}><Text style={styles.text}>Entrar</Text></TouchableOpacity>
         <View style={styles.lineBtn}>
-          
+        <TouchableOpacity style={[styles.esq, styles.borderRadius100]}><Text style={styles.text}>Esqueceu a senha?</Text></TouchableOpacity>
+        <TouchableOpacity style={[styles.cad, styles.borderRadius100]}><Text style={styles.text}>Cadastrar</Text></TouchableOpacity>
         </View>
       </View>
       <StatusBar style="auto" />
@@ -43,6 +44,32 @@ const styles = StyleSheet.create({
   ipt:{
     borderRadius: 8,
     borderWidth: 1,
-    height: 57
-  }
+    height: 57,
+    paddingLeft: 8,
+    backgroundColor: "#EFEFEF"
+  },
+  btn:{
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#8A2BE2',
+    height: 57,
+  },
+  text:{
+    color: 'white'
+  },
+  lineBtn:{
+    display: 'flex',
+    flexDirection: 'row',
+    
+  },
+  borderRadius100: {
+    borderRadius: 100,
+  },
+  esq: {
+    backgroundColor: "#EFEFEF",
+  },
+  cad:{
+    backgroundColor: "#332B3C",
+  },
 });
