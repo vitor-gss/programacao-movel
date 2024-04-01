74894cd747b117ba943ecc2043faf66fea2f44e3
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2024-04-01 09:24:43 -0300

    Adição da barra de botões

--- vixz/App.js
+++ vixz/App.js
@@ -5,18 +5,18 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text>Login</Text>
+        <Text style={styles.login}>Login</Text>
       </View>
       <View style={styles.content}>
         <View style={styles.line}>
 
         </View>
         <TextInput placeholder='E-mail ou nome de usuário' style={styles.ipt}/>
-        <TextInput placeholder='Senha' secureTextEntry="true" style={styles.ipt}/>
+        <TextInput placeholder='Senha'  style={styles.ipt}/>
         <TouchableOpacity style={[styles.btn, styles.borderRadius100]}><Text style={styles.text}>Entrar</Text></TouchableOpacity>
         <View style={styles.lineBtn}>
-        <TouchableOpacity style={[styles.esq, styles.borderRadius100]}><Text style={styles.text}>Esqueceu a senha?</Text></TouchableOpacity>
-        <TouchableOpacity style={[styles.cad, styles.borderRadius100]}><Text style={styles.text}>Cadastrar</Text></TouchableOpacity>
+        <TouchableOpacity style={[styles.esq, styles.borderRadius100, styles.btnLine]}><Text>Esqueceu a senha?</Text></TouchableOpacity>
+        <TouchableOpacity style={[styles.cad, styles.borderRadius100, styles.btnLine]}><Text style={styles.text}>Cadastrar</Text></TouchableOpacity>
         </View>
       </View>
       <StatusBar style="auto" />
@@ -25,14 +25,19 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+  login:{
+    color: 'white'
+  },
   container: {
     flex: 1,
-    /*backgroundColor: '#332B3C',*/
+    backgroundColor: '#332B3C',
     alignItems: 'center',
     justifyContent: 'center',
   },
   header:{
     marginBottom: "5%",
+    borderWidth: 1,
+    borderBottomColor: "#fff"
   },
   content:{
     display: 'flex',
@@ -61,15 +66,26 @@ const styles = StyleSheet.create({
   lineBtn:{
     display: 'flex',
     flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: "100%"
     
   },
   borderRadius100: {
     borderRadius: 100,
+  },
+  btnLine:{
+    width: "45%",
+    height: 46,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   esq: {
     backgroundColor: "#EFEFEF",
   },
   cad:{
     backgroundColor: "#332B3C",
+    borderWidth: 1,
+    borderColor: '#633C8E'
   },
 });
