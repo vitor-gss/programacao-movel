8fc60b9c59a54b24d8d39d425439cc9329d5b7f1
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2024-08-19 09:25:05 -0300

    botao sair

--- Vixzz/app/home.js
+++ Vixzz/app/home.js
@@ -1,12 +1,22 @@
 import { View, Text, Pressable } from 'react-native'
 import {auth} from '../firebaseConfig'
 import { useRouter } from 'expo-router'
+import { signOut } from "firebase/auth";
 
 export default function Home() {
     const user = auth.currentUser // Usuário logado no momento
+    const router = useRouter()
+
+    const handleLogout = async () => {
+      await signOut(auth)
+      router.replace('/')
+    }
   return (
     <View>
       <Text>{user.email}</Text>
+      <Pressable onPress={handleLogout} style={{backgroundColor: '#009cce', width: '100%', padding: 20}}>
+        <Text>Sair</Text>
+      </Pressable>
     </View>
   )
 }
