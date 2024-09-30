1c938b3ea48d86295bd6d9335def8ed54e3995e6
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2024-09-30 10:23:03 -0300

    ababa

--- Vixzz/app/home.js
+++ Vixzz/app/home.js
@@ -1,24 +1,26 @@
 import { View, Text, Pressable } from 'react-native'
-import {auth} from '../firebaseConfig'
+import { auth } from '../firebaseConfig'
 import { useRouter } from 'expo-router'
 import { signOut } from "firebase/auth";
 
 import styles from './styles/templateStyles'
 
 export default function Home() {
-    const user = auth.currentUser // Usuário logado no momento
-    const router = useRouter()
+  const user = auth.currentUser // Usuário logado no momento
+  const router = useRouter()
 
-    const handleLogout = async () => {
-      await signOut(auth)
-      router.replace('/')
-    }
+  const handleLogout = async () => {
+    await signOut(auth)
+    router.replace('/')
+  }
   return (
-    <View>
-      <Text>{user.email}</Text>
-      <Pressable onPress={handleLogout} style={{backgroundColor: '#009cce', width: '100%', padding: 20}}>
-        <Text>Sair</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <Text>{user.email}</Text>
+        <Pressable onPress={handleLogout} style={{ backgroundColor: '#009cce', width: '100%', padding: 20 }}>
+          <Text>Sair</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
