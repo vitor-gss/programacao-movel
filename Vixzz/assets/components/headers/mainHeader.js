c8840753ef78fd15dd34465dedb7359070f8d137
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2024-10-21 10:39:39 -0300

    conclusão mainHeader

--- Vixzz/assets/components/headers/mainHeader.js
+++ Vixzz/assets/components/headers/mainHeader.js
@@ -1,11 +1,13 @@
-import { View, StyleSheet, Image } from 'react-native'
+import { View, StyleSheet, Image, Text } from 'react-native'
 import * as React from "react"
 import { IconButton } from 'react-native-paper';
 
 export default function MainHeader() {
     return (
         <View style={styles.container}>
-            <Image source={require('../../logo/icon/vixz.png')} style={styles.img} />
+            {/* <View style={{ flex:1, backgroundColor: 'green' }}>
+            </View> */}
+                <Image source={require('../../logo/icon/vixz.png')} style={styles.img} />
             <View style={styles.icons}>
                 <IconButton
                     icon="bell-outline"
@@ -22,23 +24,22 @@ export default function MainHeader() {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex:0.3,
         flexDirection: 'row',
-        alignItems: "center",
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
         gap: 5,
+
         borderBottomWidth: 0.5,
         borderColor: "#633C8E",
-
-        padding: 0,
-        margin: 0
     },
     img: {
         width: 86,
         height: 40,
     },
     icons: {
-        flex: 1,
-        flexDirection: 'row',
+        flexDirection: 'row'
     }
 
 })
