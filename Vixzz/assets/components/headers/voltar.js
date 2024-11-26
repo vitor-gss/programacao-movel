b7caccc840bcab9ff6a0c2d64a81bb876055ff93
Vitor Gabriel da Silva Santos <commercial.vitorgabriel@gmail.com>
2024-11-26 11:06:59 -0300

    mudanças no botão de voltar

--- Vixzz/assets/components/headers/voltar.js
+++ Vixzz/assets/components/headers/voltar.js
@@ -1,23 +1,24 @@
-import { Pressable, View, Text, StyleSheet } from 'react-native'
-import Arrow from 'react-native-arrow'
+import { Pressable, Text, StyleSheet } from 'react-native';
+import Arrow from 'react-native-arrow';
 
 export default function voltar(props) {
   return (
     <Pressable onPress={props.onPress} style={localStyle.btn}>
-      <Arrow size={15} color={'#633C8E'} direction={"left"} style={localStyle.arrow}/>
-      <Text style={localStyle.text}>oie</Text>
+      <Arrow size={15} color={'#633C8E'} direction={"left"} />
+      <Text style={localStyle.text}>Configurações</Text>
     </Pressable>
-  )
+  );
 }
 
 const localStyle = StyleSheet.create({
   btn: {
-    flexDirection: 'row'
+    flexDirection: 'row', // Organiza a seta e o texto em linha
+    alignItems: 'center', // Centraliza verticalmente
   },
-  arrow: {
-
+  text: {
+    fontSize: 18, // Ajuste do tamanho do texto
+    fontWeight: 'bold', // Opcional, para dar mais destaque
+    color: '#633C8E', // Cor do texto
+    marginLeft: 8, // Espaço entre a seta e o texto
   },
-  text:{
-
-  }
-})
+});
