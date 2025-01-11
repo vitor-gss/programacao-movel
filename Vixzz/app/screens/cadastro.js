bfe1b4e815b20297e42ea9886b3e83b79a6c551f
Vitor Gabriel da Silva Santos <126255280+vitor-gss@users.noreply.github.com>
2025-01-11 17:30:14 -0300

    :broom: cleanup: Eliminando console.log no cadastro

--- Vixzz/app/screens/cadastro.js
+++ Vixzz/app/screens/cadastro.js
@@ -24,7 +24,6 @@ export default function Cadastro() {
     createUserWithEmailAndPassword(auth, email, senha)
       .then((userCredential) => {
         const user = userCredential.user;
-        console.log(user);
         router.replace('/screens/home')
       })
       .catch((error) => {
